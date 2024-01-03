8a1aaf3d1cdebab7145466f5cda3e622b2383032
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-03 15:14:58 +0100

    database configuration

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const DB_URI = process.env.MONGODB_URI || "";
+
+async function connectDB() {
+  try {
+    if (!DB_URI) {
+      throw new Error("MONGODB_URI is not defined in the environment");
+    }
+
+    await mongoose.connect(DB_URI);
+    console.log("Connected to MongoDB!");
+  } catch (error: any) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+export { connectDB };
