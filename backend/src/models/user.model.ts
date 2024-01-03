80e37b1df849685f07a048efe58476dab9c3b121
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-03 16:27:10 +0100

    add user schema to database

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+interface IUser {
+  name: String;
+  email: String;
+  password: String;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
