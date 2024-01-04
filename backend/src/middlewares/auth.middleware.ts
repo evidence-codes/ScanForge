a7140580f7b757dcc896eaa7f5921528195f0066
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-04 04:33:40 +0100

    feat: create middleware to authenticate requests

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface AuthenticatedRequest extends Request {
+  user?:
+    | {
+        id: string;
+      }
+    | JwtPayload;
+}
+
+const secret = process.env.JWT_SEC || " ";
+
+const auth = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers["authorization"];
+
+  if (authHeader) {
+    jwt.verify(authHeader, secret, (err, decoded) => {
+      if (err) {
+        throw new Error("Token is not valid!");
+      } else {
+        if (typeof decoded === "string") {
+          req.user = { id: decoded }; // Assigning user ID directly to req.user
+        } else {
+          req.user = decoded as JwtPayload; // Assigning JwtPayload to req.user
+        }
+        next();
+      }
+    });
+  } else {
+    throw new Error("You are not authenticated!");
+  }
+};
+
+export default auth;
