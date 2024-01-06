3d595fad8c4d24cfa2821ffde556a4c52c349068
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-06 02:40:15 +0100

    refactor: diff change password from forgot password

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -9,6 +9,7 @@ import auth from "../middlewares/auth.middleware";
 const router = Router();
 
 router.get("/get-info", auth, getUserInfo);
+router.patch("/forgot-password", forgotPassword);
 router.patch("/change-password", auth, forgotPassword);
 router.delete("/delete", auth, deleteUser);
 
