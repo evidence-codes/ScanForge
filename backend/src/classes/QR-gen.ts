35062357e37f392638cb3fbb1425493275046488
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-06 02:33:37 +0100

    feat: add history and delete feature to QR class

--- backend/src/classes/QR-gen.ts
+++ backend/src/classes/QR-gen.ts
@@ -5,24 +5,36 @@ class QR_gen {
   async generate(data: IQr) {
     try {
       const { userId, url } = data;
-      const qr_svg = qr.image(url.toString(), { type: "png", size: 30 });
-      const chunks: any = [];
+      const qr_svg = qr.imageSync(url.toString(), { type: "png", size: 30 });
+      const base64QR = qr_svg.toString("base64");
 
-      qr_svg.on("data", (chunk) => {
-        chunks.push(chunk);
-      });
+      const qrCode = new QR({ userId, url, image: base64QR });
+      await qrCode.save();
 
-      qr_svg.on("end", async () => {
-        const qrImageData = Buffer.concat(chunks);
-        const base64QR = qrImageData.toString("base64");
-
-        const qrCode = new QR({ userId, url, image: base64QR });
-        await qrCode.save();
-
-        return qrCode;
-      });
+      return qrCode;
     } catch (err) {
       throw new Error("Failed to generate QR code");
+    }
+  }
+
+  async history(id: String) {
+    try {
+      const qrs = await QR.find({ userId: id });
+      return qrs;
+    } catch (err: any) {
+      throw new Error("Failed to get codes...");
+    }
+  }
+
+  async delete(id: String) {
+    try {
+      const deleted = await QR.findByIdAndDelete(id);
+      if (!deleted) {
+        throw new Error("QR code not found");
+      }
+      return deleted;
+    } catch (err: any) {
+      throw new Error("Could not delete qr code");
     }
   }
 }
