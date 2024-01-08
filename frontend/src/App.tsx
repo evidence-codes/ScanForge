2b9a3aa4ab0771ebfe0deb67e83af7056777a5ab
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-08 14:40:34 +0100

    render qr component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Qrcode from "./components/Qrcode";
 
 function App() {
   return (
     <>
       <Header />
+      <Qrcode />
     </>
   );
 }
