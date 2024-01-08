49ade08b0be91f6530765abeaf272d6eaefd0c28
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-08 15:26:31 +0100

    feat: unresponsive style

--- frontend/src/components/Qrcode.tsx
+++ frontend/src/components/Qrcode.tsx
@@ -1,27 +1,56 @@
+import { useState } from "react";
 import QR from "../assets/qr.svg";
 import Button from "./Button";
 
 function Qrcode() {
+  const [selectedFormat, setSelectedFormat] = useState("");
+
+  const handleClick = (format: string) => {
+    setSelectedFormat(format);
+  };
   return (
     <>
       <main>
         <div className="main-gradient flex flex-col items-center pt-16 pb-20">
           <div>
-            <h1 className="">Swift QR Generation</h1>
-            <h2>Instantly encode your data into codes</h2>
+            <h1 className="text-white font-bold sm:text-7xl">
+              ScanForge QR Generation
+            </h1>
+            <h2 className="text-center text-white sm:text-4xl font-semibold">
+              Instantly encode your data into codes
+            </h2>
           </div>
-          <div>
-            <p>Enter Link or Text</p>
-            <input type="search" name="" id="" placeholder="https://" />
-            <ul className="flex">
-              <li>
-                <a href="">PNG</a>
+          <div className="p-4">
+            <p className="text-white text-sm font-bold">Enter Link or Text</p>
+            <input
+              type="text"
+              placeholder="https://"
+              className="p-4 input-width rounded-xl  ... outline-transparent"
+            />
+            <ul className="flex space-x-2 text-white font-semibold text-sm">
+              <li
+                className={`cursor-pointer ${
+                  selectedFormat === "PNG" ? "underline decoration-2" : ""
+                }`}
+                onClick={() => handleClick("PNG")}
+              >
+                PNG
               </li>
-              <li>
-                <a href="">JPG</a>
+              <li
+                className={`cursor-pointer ${
+                  selectedFormat === "JPG" ? "underline decoration-2" : ""
+                }`}
+                onClick={() => handleClick("JPG")}
+              >
+                JPG
               </li>
-              <li>
-                <a href="">SVG</a>
+              <li
+                className={`cursor-pointer ${
+                  selectedFormat === "SVG" ? "underline decoration-2" : ""
+                }`}
+                onClick={() => handleClick("SVG")}
+              >
+                SVG
               </li>
             </ul>
           </div>
