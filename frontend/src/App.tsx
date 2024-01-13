eeafdcccc16f2ad361b50c051326f47e77b4721d
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-13 11:33:36 +0100

    feat: add step component to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Qrcode from "./components/Qrcode";
+import Step from "./components/Steps";
 
 function App() {
   return (
     <>
       <Header />
       <Qrcode />
+      <Step />
+      <Footer />
     </>
   );
 }
