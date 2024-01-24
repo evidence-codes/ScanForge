cfe42d8df6b50e0c58a6b99c7ed378bb2112b425
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-24 23:04:04 +0100

    feat: display forms on test mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import "./App.css";
-import Download from "./components/Download";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Qrcode from "./components/Qrcode";
-import Step from "./components/Steps";
+import Login from "./pages/Login";
+// import Download from "./components/Download";
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Qrcode from "./components/Qrcode";
+// import Step from "./components/Steps";
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header />
       <Qrcode />
       <Step />
       <Download />
-      <Footer />
+      <Footer /> */}
+      <Login />
     </>
   );
 }
