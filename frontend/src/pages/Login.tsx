9815d1c10d29de655c8457084c78dd11aba8ea67
CybertronicEvidence <adejuwonevidence181@gmail.com>
2024-01-25 00:00:28 +0100

    feat: create auth page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,9 +2,17 @@ import Form from "../components/Form";
 
 function Login() {
   return (
-    <>
-      <Form />
-    </>
+    <div className="flex flex-col md:flex-row h-screen">
+      {/* Left side (header on smaller screens) */}
+      <div className="w-full md:w-1/2 sm:grad-pic main-gradient p-4 text-white flex items-center justify-center">
+        <p className="text-center font-bold text-3xl sm:text-5xl">ScanForge</p>
+      </div>
+
+      {/* Right side with form */}
+      <div className="w-full md:w-1/2 flex items-center justify-center">
+        <Form />
+      </div>
+    </div>
   );
 }
 
